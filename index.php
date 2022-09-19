3f9bd7425fc0c78958ec1c52d4ecfb696d5fb39f
carda114 <113865321+carda114@users.noreply.github.com>
2022-09-18 21:33:28 -0400

    Update index.php
    Updated version

--- index.php
+++ index.php
@@ -8,12 +8,65 @@ $looking = isset($_GET['title']) || isset($_GET['author']);
 	<title>Bookstore</title>
 </head>
 <body>
-	<p>You are <?php echo $_COOKIE['username']; ?></p>
-	<p>You lookin'? <?php echo (int) $looking; ?></p>
-	<p>The book you are looking for is</p>
+	<p>
+		<?php 
+		if (isset($_COOKIE['username'])) {
+			echo "You are " . $_COOKIE['username'];
+		} else {
+			echo "You are not authenticated.";
+		}
+		?>
+	</p>
+	<?php
+	if (isset($_GET['title']) && isset($_GET['author'])) {
+	?>
+		<p>The book you are looking for is</p>
+		<ul>
+			<li><b>Title</b>: <?php echo $_GET['title']; ?></li>
+			<li><b>Author</b>: <?php echo $_GET['author']; ?></li>
+		</ul>
+	<?php
+	} else {
+	?>
+		<p>You are not looking for a book?</p>
+	<?php
+	}
+	?>
+	<?php
+		$books = [
+			[
+				'title' => 'To Kill A Mockingbird',
+				'author' => 'Harper Lee',
+				'available' => true,
+				'pages' => 336,
+				'isbn' => 9780061120084
+			],
+			[
+				'title' => '1984',
+				'author' => 'George Orwell',
+				'available' => true,
+				'pages' => 267,
+				'isbn' => 9780547249643
+			],
+			[
+				'title' => 'One Hundred Years Of Solitude',
+				'author' => 'Gabriel Garcia Marquez',
+				'available' => false,
+				'pages' => 267,
+				'isbn' => 9785267006323
+			],
+		];
+	?>
 	<ul>
-		<li><b>Title</b>: <?php echo $_GET['title']; ?></li>
-		<li><b>Author</b>: <?php echo $_GET['author']; ?></li>
+	<?php foreach ($books as $book): ?>
+		<li>
+			<i><?php echo $book['title']; ?></i>
+			- <?php echo $book['author']; ?>
+			<?php if (!$book['available']): ?>
+				<b>Not available</b>
+			<?php endif; ?>
+		</li>
+	<?php endforeach; ?>
 	</ul>
 </body>
 </html>
